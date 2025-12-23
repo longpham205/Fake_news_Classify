11187ddc1fad6cb926bf7339a5ddc10ab1f55e8a
Long <longp8741@gmail.com>
2025-12-23 16:43:48 +0700

    up

--- main/run_training.py
+++ main/run_training.py
@@ -3,7 +3,7 @@ import sys
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, root_dir)
 
-from src.main.run_preprocessing import run_preprocessing
+from main.run_preprocessing import run_preprocessing
 from src.train.train import train
 from src.train.evaluate import evaluate
 
